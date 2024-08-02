1e0c39b062105c528323ea11c2fc98e9fc461554
Vipul <vipuldamrepro26@gmail.com>
2024-08-02 08:33:10 +0530

    assignment complete

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import FetchQuote from './components/FetchQuote/FetchQuote';
+
+function App() {
+
+  return (
+    <div className='app'>
+      <FetchQuote />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/FetchQuote/FetchQuote.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react'
+import './FetchAdvice.css';
+import axios from 'axios';
+import { FaDiceFive } from "react-icons/fa";
+import { CiPause1 } from "react-icons/ci";
+
+const FetchAdvice = () => {
+
+  const [data, setData] = useState('');
+
+  const apiURL = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes';
+
+
+  const fetchAdvice = async () => {
+    try {
+      const res = await axios.get(apiURL);
+      // console.log(res.data[0]);
+      setData(res.data[0])
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchAdvice();
+  }, [])
+
+
+  return (
+    <div className='fetch-advice'>
+      <div className="fetch-main">
+        <div className="id">
+          <p>QUOTE</p>
+        </div>
+        <p>"{data}"</p>
+        <CiPause1 className='icon1'/>
+        <div className="icon2">
+          <FaDiceFive className='icon' onClick={fetchAdvice} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FetchAdvice
